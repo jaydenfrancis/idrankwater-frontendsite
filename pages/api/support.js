23d175298d944b5f81d6a9ad8348a01aeba38167
jaydenfrancis <jayden@uchicago.edu>
2024-05-25 15:49:58 -0500

    support form

--- pages/api/support.js
+++ pages/api/support.js
@@ -1,35 +1,30 @@
+const sgMail = require('@sendgrid/mail');
+
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
-      const { email, question } = req.body;
-  
-      // Send email with the support request
-      const nodemailer = require('nodemailer');
-      const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
-        },
-      });
-  
-      const mailOptions = {
-        from: process.env.EMAIL_USER,
-        to: process.env.EMAIL_RECIEVER,
-        subject: 'New Support Request',
-        text: `
-          Email: ${email}
-          Question: ${question}
-        `,
-      };
-  
-      try {
-        await transporter.sendMail(mailOptions);
-        res.status(200).json({ message: 'Support request sent successfully' });
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'An error occurred while sending the support request' });
-      }
-    } else {
-      res.status(405).json({ message: 'Method not allowed' });
+  if (req.method === 'POST') {
+    const { email, question } = req.body;
+
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const msg = {
+      to: 'jaydencolefrancis@gmail.com',
+      from: 'jaydencolefrancis@gmail.com',
+      subject: 'New Support Request',
+      text: `
+        Email: ${email}
+        Question: ${question}
+      `,
+    };
+    console.log('Email:', email, 'Question:', question);
+
+    try {
+      await sgMail.send(msg);
+      res.status(200).json({ message: 'Support request sent successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'An error occurred while sending the support request' });
     }
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
   }
+}
