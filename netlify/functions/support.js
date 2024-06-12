1b456ed10b1770e166b05a87b8f6842cb7527997
jaydenfrancis <jayden@uchicago.edu>
2024-06-12 10:58:43 -0700

    support fix I hope

---
+++ netlify/functions/support.js
@@ -0,0 +1,40 @@
+// netlify/functions/support.js
+const sgMail = require('@sendgrid/mail');
+
+exports.handler = async function(event, context) {
+  if (event.httpMethod === 'POST') {
+    const { email, question } = JSON.parse(event.body);
+
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const msg = {
+      to: 'jaydencolefrancis@gmail.com',
+      from: 'jaydencolefrancis@gmail.com',
+      subject: 'New Support Request',
+      text: `
+        Email: ${email}
+        Question: ${question}
+      `,
+    };
+    console.log('Email:', email, 'Question:', question);
+
+    try {
+      await sgMail.send(msg);
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Support request sent successfully' }),
+      };
+    } catch (error) {
+      console.error(error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ message: 'An error occurred while sending the support request' }),
+      };
+    }
+  } else {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ message: 'Method not allowed' }),
+    };
+  }
+};
